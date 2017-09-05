bab8b65e489f886fd81c53d4bca3bc2fdb2fbb69
ricky yuliadi <ricky.yuliadi@gmail.com>
2017-09-05 15:38:45 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>::SIM Kotaku Login::</title>
+    <title>::Kotaku Login::</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/favicon.ico"/>
     <!-- Bootstrap -->
