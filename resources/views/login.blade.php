8d44066d05c3cd8c1643a431ea4c26cdf2b51a99
ricky yuliadi <ricky.yuliadi@gmail.com>
2017-09-05 16:11:05 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,27 +2,27 @@
 <html>
 
 <head>
-    <title>::Admin Login::</title>
+    <title>::Admin Register::</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/favicon.ico"/>
-    <!-- Bootstrap -->
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <!-- end of bootstrap -->
+    <!-- global css -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <!-- end of global css -->
     <!--page level css -->
     <link type="text/css" href="{{asset('vendors/themify/css/themify-icons.css')}}" rel="stylesheet"/>
     <link href="{{asset('vendors/iCheck/css/all.css')}}" rel="stylesheet">
     <link href="{{asset('vendors/bootstrapvalidator/css/bootstrapValidator.min.css')}}" rel="stylesheet"/>
     <link href="{{asset('css/login.css')}}" rel="stylesheet">
-    <!--end page level css-->
+    <!--end of page level css-->
 </head>
 
-<body id="sign-in">
+<body id="sign-up">
 <div class="preloader">
     <div class="loader_img"><img src="{{asset('img/loader.gif')}}" alt="loading..." height="64" width="64"></div>
 </div>
 <div class="container">
     <div class="row">
-        <div class="col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 login-form">
+        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1 login-form">
             <div class="panel-header">
                 <h2 class="text-center">
                     <img src="{{asset('img/pages/clear_black.png')}}" alt="Logo">
@@ -30,50 +30,93 @@
             </div>
             <div class="panel-body">
                 <div class="row">
-                    <div class="col-xs-12">
-                        <form action="{{ route('login') }}" id="authentication" method="post" class="login_validator">
-                            {{ csrf_field() }}
+                    <form action="{{ route('register') }}" id="authentication" method="post" class="signup_validator">
+                        {{ csrf_field() }}
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="first" class="sr-only">First Name</label>
+                                <input type="text" class="form-control  form-control-lg" id="name" name="name"
+                                       placeholder="First name" value="{{ old('name') }}" required autofocus>
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="last" class="sr-only">Last Name</label>
+                                <input type="text" class="form-control  form-control-lg" id="last" name="last_name"
+                                       placeholder="Last name">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
                             <div class="form-group">
                                 <label for="email" class="sr-only"> E-mail</label>
-                                <input type="text" class="form-control  form-control-lg" id="email" name="email"
-                                       placeholder="E-mail">
+                                <input id="email" type="email" class="form-control  form-control-lg" name="email" value="{{ old('email') }}" placeholder="E-mail" required>
+
+                                <!-- <input type="text" class="form-control  form-control-lg" id="email" name="email" placeholder="E-mail"> -->
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
                             </div>
+                        </div>
+                        <div class="col-md-12">
                             <div class="form-group">
                                 <label for="password" class="sr-only">Password</label>
                                 <input type="password" class="form-control form-control-lg" id="password"
-                                       name="password" placeholder="Password">
+                                       name="password" placeholder="Password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
                             </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="confirm-password" class="sr-only">Password</label>
+                                <!-- <input type="password" class="form-control form-control-lg" id="confirm-password" name="password_confirm" placeholder="Confirm Password"> -->
+                                <input id="password-confirm" type="password" class="form-control form-control-lg" name="password_confirmation" placeholder="Confirm Password" required>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
                             <div class="form-group checkbox">
-                                <label for="remember">
-                                    <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>&nbsp; Remember Me
+                                <label for="terms">
+                                    <input type="checkbox" name="terms" id="terms">&nbsp; I accept the <a href="javascript:void(0)">terms &amp; Conditions</a>
                                 </label>
                             </div>
+                        </div>
+                        <div class="col-md-12">
                             <div class="form-group">
-                                <input type="submit" value="Sign In" class="btn btn-primary btn-block"/>
+                                <input type="submit" value="Sign Up" class="btn btn-primary btn-block"/>
                             </div>
-                            <a href="forgot_password.html" id="forgot" class="forgot"> Lupa Password</a>
-                            <span class="pull-right sign-up"><a href="register">Daftar Baru</a></span>
-                        </form>
-                    </div>
+                            <span class="sign-in">Already a member? <a href="login">Sign In</a></span>
+                        </div>
+                    </form>
                 </div>
                 <div class="row text-center social">
                     <div class="col-xs-12">
-                        <p class="alter">Sign in with</p>
+                        <p class="alter">Sign Up with</p>
                     </div>
                     <div class="row">
                         <div class="col-sm-8 col-sm-offset-2">
                             <div class="col-xs-4">
-                                <a href="#" class="btn btn-lg btn-facebook">
+                                <a href="javascript:void(0)" class="btn btn-lg btn-facebook">
                                     <i class="ti-facebook"></i>
                                 </a>
                             </div>
                             <div class="col-xs-4">
-                                <a href="#" class="btn btn-lg btn-twitter">
+                                <a href="javascript:void(0)" class="btn btn-lg btn-twitter">
                                     <i class="ti-twitter-alt"></i>
                                 </a>
                             </div>
                             <div class="col-xs-4">
-                                <a href="#" class="btn btn-lg btn-google">
+                                <a href="javascript:void(0)" class="btn btn-lg btn-google">
                                     <i class="ti-google"></i>
                                 </a>
                             </div>
@@ -88,10 +131,13 @@
 <script src="{{asset('js/jquery.min.js')}}" type="text/javascript"></script>
 <script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
 <!-- end of global js -->
-<!-- page level js -->
-<script type="text/javascript" src="{{asset('vendors/iCheck/js/icheck.js')}}"></script>
+<!-- begining of page level js -->
+<script src="{{asset('vendors/moment/js/moment.min.js')}}"></script>
+<script src="{{asset('vendors/bootstrap-datepicker/js/bootstrap-datepicker.js')}}"></script>
+<script src="{{asset('vendors/select2/js/select2.js')}}"></script>
+<script src="{{asset('vendors/iCheck/js/icheck.js')}}"></script>
 <script src="{{asset('vendors/bootstrapvalidator/js/bootstrapValidator.min.js')}}" type="text/javascript"></script>
-<script type="text/javascript" src="{{asset('js/custom_js/login.js')}}"></script>
+<script src="{{asset('js/custom_js/register.js')}}"></script>
 <!-- end of page level js -->
 </body>
 
