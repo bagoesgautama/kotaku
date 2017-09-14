22ce98ccbbe353737bf0d36d365a9eeb152e8777
ricky yuliadi <ricky.yuliadi@gmail.com>
2017-09-14 11:43:02 +0700

    update

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-		Spatie\Permission\PermissionServiceProvider::class,
 	    Laracasts\Flash\FlashServiceProvider::class,
 	    Collective\Html\HtmlServiceProvider::class,
     ],
