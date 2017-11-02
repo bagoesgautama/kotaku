49285e7167ea3ab6ea0b741dc463c40092011ed4
ricky yuliadi <ricky.yuliadi@gmail.com>
2017-11-02 17:23:51 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -104,13 +104,13 @@ Route::get('/hrm/management/registrasi_manual', 'HRM\bk020111Controller@index');
 Route::post('hrm/management/registrasi_manual', 'HRM\bk020111Controller@post');
 Route::get('/hrm/management/registrasi_manual/create', 'HRM\bk020111Controller@create');
 Route::post('/hrm/management/registrasi_manual/create', 'HRM\bk020111Controller@post_create');
-Route::get('/hrm/admin/registrasi_manual/delete', 'HRM\bk020111Controller@delete');
+Route::get('/hrm/management/registrasi_manual/delete', 'HRM\bk020111Controller@delete');
 
 Route::get('/hrm/management/persetujuan', 'HRM\bk020116Controller@index');
 Route::post('hrm/management/persetujuan', 'HRM\bk020116Controller@post');
 Route::get('/hrm/management/persetujuan/create', 'HRM\bk020116Controller@create');
 Route::post('/hrm/management/persetujuan/create', 'HRM\bk020116Controller@post_create');
-Route::get('/hrm/admin/persetujuan/delete', 'HRM\bk020116Controller@delete');
+Route::get('/hrm/management/persetujuan/delete', 'HRM\bk020116Controller@delete');
 
 Route::get('/hrm/management/user/blacklist', 'HRM\bk020308Controller@index');
 Route::post('/hrm/management/user/blacklist', 'HRM\bk020308Controller@post');
