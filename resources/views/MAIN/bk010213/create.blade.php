dc656db231f722b8559b08783e28076fb61b25aa
ricky yuliadi <ricky.yuliadi@gmail.com>
2017-11-14 15:32:18 +0700

    update

--- resources/views/MAIN/bk010213/create.blade.php
+++ resources/views/MAIN/bk010213/create.blade.php
@@ -9,6 +9,7 @@
 <link href="{{asset('vendors/selectric/css/selectric.css')}}" rel="stylesheet" type="text/css">
 <link href="{{asset('vendors/selectize/css/selectize.bootstrap3.css')}}" rel="stylesheet" type="text/css">
 <link href="{{asset('vendors/bootstrap-fileinput/css/fileinput.min.css')}}" media="all" rel="stylesheet" type="text/css"/>
+<link href="{{asset('vendors/bootstrapvalidator/css/bootstrapValidator.min.css')}}" media="all" rel="stylesheet" type="text/css"/>
 @stop {{-- Page Header--}} @section('page-header')
 <!-- Content Header (Page header) -->
 <section class="content-header">
@@ -47,7 +48,7 @@
                                     <input type="number" id="tahun-input" name="tahun-input" class="form-control" placeholder="Tahun" value="{{$tahun}}" required maxlength="4">
                                 </div>
                             </div>
-                            <div class="form-group striped-col">
+                            <div class="form-group ">
                                 <label class="col-sm-3 control-label">Tingkat Forum</label>
                                 <div class="col-sm-6">
                                     <select id="tk-forum-input" name="tk-forum-input" class="form-control" size="1" required>
@@ -56,7 +57,7 @@
                                     </select>
                                 </div>
                             </div>
-                            <div class="form-group striped-col">
+                            <!--<div class="form-group striped-col">
                                 <label class="col-sm-3 control-label">KMW</label>
                                 <div class="col-sm-6">
                                     <select id="select-kode-kmw-input" name="kode-kmw-input" class="form-control select2" size="1" required>
@@ -66,7 +67,7 @@
                                         @endforeach
                                     </select>
                                 </div>
-                            </div>
+                            </div>-->
                             <div class="form-group striped-col">
                                 <label class="col-sm-3 control-label">Kota</label>
                                 <div class="col-sm-6">
@@ -80,7 +81,7 @@
                                     </select>
                                 </div>
                             </div>
-                            <div class="form-group striped-col">
+                            <!--<div class="form-group striped-col">
                                 <label class="col-sm-3 control-label">Korkot</label>
                                 <div class="col-sm-6">
                                     <select id="select-kode-korkot-input" name="kode-korkot-input" class="form-control select2" size="1" required>
@@ -92,8 +93,8 @@
                                         @endif
                                     </select>
                                 </div>
-                            </div>
-                            <div class="form-group striped-col">
+                            </div>-->
+                            <div class="form-group ">
                                 <label class="col-sm-3 control-label">Kecamatan</label>
                                 <div class="col-sm-6">
                                     <select id="select-kode-kec-input" name="kode-kec-input" class="form-control select2" size="1" required>
@@ -106,43 +107,36 @@
                                     </select>
                                 </div>
                             </div>
-                            <div class="form-group striped-col">
+                            <!--<div class="form-group striped-col">
                                 <label class="col-sm-3 control-label">Jenis kegiatan</label>
                                 <div class="col-sm-6">
                                     <select id="jns-kegiatan-input" name="jns-kegiatan-input" class="form-control" size="1" required>
-                                        <!-- <option value="2.4.5" {!! $jenis_kegiatan=='2.4.5' ? 'selected':'' !!}>Kolaborasi Kota</option> -->
                                         <option value="2.4.6" {!! $jenis_kegiatan=='2.4.6' ? 'selected':'' !!}>Kolaborasi Kecamatan</option>
                                     </select>
                                 </div>
-                            </div>
+                            </div>-->
                             <div class="form-group striped-col">
-                                <label class="col-sm-3 control-label" for="example-text-input1">Tanggal Kegiatan</label>
+                                <label class="col-sm-3 control-label" for="example-text-input1">Tanggal Pembentukan</label>
                                 <div class="col-sm-6">
                                     <input class="form-control" id="tgl-kegiatan-input" name="tgl-kegiatan-input" placeholder="Tanggal Kegiatan" data-provide="datepicker" data-date-format="yyyy-mm-dd" value="{{$tgl_kegiatan}}" required>
                                 </div>
                             </div>
-                            <div class="form-group striped-col">
-                                <label class="col-sm-3 control-label" for="example-text-input1">Lokasi Kegiatan</label>
-                                <div class="col-sm-6">
-                                    <input type="text" id="lok-kegiatan-input" name="lok-kegiatan-input" class="form-control" value="{{$lok_kegiatan}}" maxlength="50" required>
-                                </div>
-                            </div>
-                            <div class="form-group striped-col">
+                            <div class="form-group ">
                                 <label class="col-sm-3 control-label" for="kode">Anggota Laki-laki</label>
                                 <div class="col-sm-6">
-                                    <input type="number" id="q-laki-input" name="q-laki-input" class="form-control" placeholder="Jumlah" value="{{$q_anggota_p}}" required>
+                                    <input type="number" id="q-laki-input" name="q-laki-input" class="form-control" placeholder="Jumlah" value="{{$q_anggota_p}}" required data-bv-callback="true" data-bv-callback-message="pria & wanita tidak boleh 0" data-bv-callback-callback="check">
                                 </div>
                             </div>
                             <div class="form-group striped-col">
                                 <label class="col-sm-3 control-label" for="kode">Anggota Perempuan</label>
                                 <div class="col-sm-6">
-                                    <input type="number" id="q-perempuan-input" name="q-perempuan-input" class="form-control" placeholder="Jumlah" value="{{$q_anggota_w}}" required>
+                                    <input type="number" id="q-perempuan-input" name="q-perempuan-input" class="form-control" placeholder="Jumlah" value="{{$q_anggota_w}}" required data-bv-callback="true" data-bv-callback-message="pria & wanita tidak boleh 0" data-bv-callback-callback="check">
                                 </div>
                             </div>
-                            <div class="form-group striped-col">
+                            <div class="form-group ">
                                 <label class="col-sm-3 control-label" for="kode">Anggota BKM</label>
                                 <div class="col-sm-6">
-                                    <input type="number" id="q-bkm-input" name="q-bkm-input" class="form-control" placeholder="Jumlah" value="{{$q_anggota_bkm}}" required>
+                                    <input type="number" id="q-bkm-input" name="q-bkm-input" class="form-control" placeholder="Jumlah" value="{{$q_anggota_bkm}}" required data-bv-callback="true" data-bv-callback-message="Jumlah BKM melebihi total pria & wanita" data-bv-callback-callback="check">
                                 </div>
                             </div>
                             <div class="form-group striped-col">
@@ -153,7 +147,7 @@
                                     <button type="button" class="btn btn-warning btn-modify" id="uploaded-file-dokumen" value="{{$uri_img_document}}" {!! $uri_img_document==null ? 'style="display:none"':'' !!}>{{$uri_img_document}}</button>
                                 </div>
                             </div>
-                            <div class="form-group striped-col">
+                            <div class="form-group ">
                                 <label class="col-sm-3 control-label">File Absensi</label>
                                 <div class="col-sm-6">
                                     <input id="file-absensi-input" type="file" class="file" data-show-preview="false" name="file-absensi-input">
@@ -223,6 +217,26 @@
 @stop
 {{-- local scripts --}} @section('footer_scripts')
 <script>
+	function check(value, validator) {
+		for(var i=0;i<prop.length;i++){
+			if(prop[i].kode==$('#select-kode-pokja-input').val()){
+				prop=prop[i];
+				break;
+			}
+		}
+		var p = parseInt($('#q-laki-input').val());
+		var w = parseInt($('#q-perempuan-input').val());
+
+		var bkm = parseInt($('#q-bkm-input').val());
+		var sum = p+w;
+		var res = true;
+		if(bkm>sum){
+			res=false;
+		}else if(p==0 && w==0){
+			res=false;
+		}
+		return res;
+	};
       $(document).ready(function () {
 	  	$("#file-dokumen-input").fileinput({
 	        showUpload: false
@@ -230,54 +244,63 @@
   		$("#file-absensi-input").fileinput({
   	        showUpload: false
   	    });
-        $('#form').on('submit', function (e) {
-            var file_dokumen = document.getElementById('file-dokumen-input').files[0];
-            var file_absensi = document.getElementById('file-absensi-input').files[0];
-            var form_data = new FormData();
-            form_data.append('kode', $('#kode').val());
-            form_data.append('file-dokumen-input', file_dokumen);
-            form_data.append('file-absensi-input', file_absensi);
-            form_data.append('uploaded-file-dokumen', $('#uploaded-file-dokumen').val());
-            form_data.append('uploaded-file-absensi', $('#uploaded-file-absensi').val());
-            form_data.append('tahun-input', $('#tahun-input').val());
-            form_data.append('tk-forum-input', $('#tk-forum-input').val());
-            form_data.append('kode-kota-input', $('#select-kode-kota-input').val());
-            form_data.append('kode-kec-input', $('#select-kode-kec-input').val());
-            form_data.append('kode-kmw-input', $('#select-kode-kmw-input').val());
-            form_data.append('kode-korkot-input', $('#select-kode-korkot-input').val());
-            form_data.append('jns-kegiatan-input', $('#jns-kegiatan-input').val());
-            form_data.append('tgl-kegiatan-input', $('#tgl-kegiatan-input').val());
-            form_data.append('lok-kegiatan-input', $('#lok-kegiatan-input').val());
-            form_data.append('q-laki-input', $('#q-laki-input').val());
-            form_data.append('q-perempuan-input', $('#q-perempuan-input').val());
-            form_data.append('q-bkm-input', $('#q-bkm-input').val());
-            form_data.append('tgl-diser-input', $('#tgl-diser-input').val());
-            form_data.append('diser-oleh-input', $('#diser-oleh-input').val());
-            form_data.append('tgl-diket-input', $('#tgl-diket-input').val());
-            form_data.append('diket-oleh-input', $('#diket-oleh-input').val());
-            form_data.append('tgl-diver-input', $('#tgl-diver-input').val());
-            form_data.append('diver-oleh-input', $('#diver-oleh-input').val());
-          e.preventDefault();
-          $.ajax({
-            type: 'post',
-            processData: false,
-            contentType: false,
-            "url": "/main/persiapan/kecamatan/kolaborasi/create",
-            data: form_data,
-            beforeSend: function (){
-                $("#submit").prop('disabled', true);
-            },
-            success: function () {
-            alert('From Submitted.');
-            window.location.href = "/main/persiapan/kecamatan/kolaborasi";
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-            alert(xhr.status);
-            alert(thrownError);
-            $("#submit").prop('disabled', false);
-            }
-          });
-        });
+		$('#form').bootstrapValidator().on('success.form.bv', function(e) {
+	        $('#form').on('submit', function (e) {
+	            var file_dokumen = document.getElementById('file-dokumen-input').files[0];
+	            var file_absensi = document.getElementById('file-absensi-input').files[0];
+	            var form_data = new FormData();
+	            form_data.append('kode', $('#kode').val());
+	            form_data.append('file-dokumen-input', file_dokumen);
+	            form_data.append('file-absensi-input', file_absensi);
+	            form_data.append('uploaded-file-dokumen', $('#uploaded-file-dokumen').val());
+	            form_data.append('uploaded-file-absensi', $('#uploaded-file-absensi').val());
+	            form_data.append('tahun-input', $('#tahun-input').val());
+	            form_data.append('tk-forum-input', $('#tk-forum-input').val());
+	            form_data.append('kode-kota-input', $('#select-kode-kota-input').val());
+	            form_data.append('kode-kec-input', $('#select-kode-kec-input').val());
+	            form_data.append('kode-kmw-input', $('#select-kode-kmw-input').val());
+	            form_data.append('kode-korkot-input', $('#select-kode-korkot-input').val());
+	            form_data.append('jns-kegiatan-input', $('#jns-kegiatan-input').val());
+	            form_data.append('tgl-kegiatan-input', $('#tgl-kegiatan-input').val());
+	            form_data.append('q-laki-input', $('#q-laki-input').val());
+	            form_data.append('q-perempuan-input', $('#q-perempuan-input').val());
+	            form_data.append('q-bkm-input', $('#q-bkm-input').val());
+	            form_data.append('tgl-diser-input', $('#tgl-diser-input').val());
+	            form_data.append('diser-oleh-input', $('#diser-oleh-input').val());
+	            form_data.append('tgl-diket-input', $('#tgl-diket-input').val());
+	            form_data.append('diket-oleh-input', $('#diket-oleh-input').val());
+	            form_data.append('tgl-diver-input', $('#tgl-diver-input').val());
+	            form_data.append('diver-oleh-input', $('#diver-oleh-input').val());
+	          e.preventDefault();
+	          $.ajax({
+	            type: 'post',
+	            processData: false,
+	            contentType: false,
+	            "url": "/main/persiapan/kecamatan/kolaborasi/create",
+	            data: form_data,
+	            beforeSend: function (){
+	                $("#submit").prop('disabled', true);
+	            },
+	            success: function () {
+	            alert('From Submitted.');
+	            window.location.href = "/main/persiapan/kecamatan/kolaborasi";
+	            },
+	            error: function (xhr, ajaxOptions, thrownError) {
+	            alert(xhr.status);
+	            alert(thrownError);
+	            $("#submit").prop('disabled', false);
+	            }
+	          });
+	        });
+		}).on('error.form.bv', function(e) {
+			$("#submit").prop('disabled', false);
+		});
+		$('#tgl-kegiatan-input')
+			.on('changeDate show', function(e) {
+				// Revalidate the date when user change it
+				$('#form').bootstrapValidator('revalidateField', 'tgl-kegiatan-input');
+				$("#submit").prop('disabled', false);
+		});
         $("#select-kode-kota-input").select2({
             theme: "bootstrap",
             placeholder: "Please Select"
@@ -382,5 +405,5 @@
 <script src="{{asset('vendors/selectric/js/jquery.selectric.min.js')}}" type="text/javascript"></script>
 <script src="{{asset('js/custom_js/custom_elements.js')}}" type="text/javascript"></script>
 <script src="{{asset('vendors/bootstrap-fileinput/js/fileinput.min.js')}}" type="text/javascript"></script>
-
+<script src="{{asset('vendors/bootstrapvalidator/js/bootstrapValidator.min.js')}}" type="text/javascript"></script>
 @stop
